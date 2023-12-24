07e034d50d14439a310060c8df54134f933fb026
rezalfara <reza.alfara07@gmail.com>
2023-12-25 01:45:29 +0700

    commit

--- handler/book.go
+++ handler/book.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"pustaka-api/book"
 
@@ -18,35 +19,48 @@ func NewBookHandler(bookService book.Service) *bookHandler {
 	return &bookHandler{bookService}
 }
 
-func (h *bookHandler) RootHandler(ctx *gin.Context) {
+func (h *bookHandler) GetBooks(ctx *gin.Context) {
+	books, err := h.bookService.FindAll()
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	var booksResponse []book.BookResponse
+
+	for _, b := range books {
+		bookResponse := convertToBookRespone(b)
+
+		booksResponse = append(booksResponse, bookResponse)
+	}
+
 	ctx.JSON(http.StatusOK, gin.H{
-		"name": "Reza Alfara",
-		"bio":  "A Software Engineer",
+		"data": booksResponse,
 	})
 }
 
-func (h *bookHandler) HelloHandler(ctx *gin.Context) {
+func (h *bookHandler) GetBook(ctx *gin.Context) {
+	idString := ctx.Param("id")
+	id, _ := strconv.Atoi(idString)
+
+	b, err := h.bookService.FindByID(id)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"errors": err,
+		})
+		return
+	}
+
+	bookResponse := convertToBookRespone(b)
+
 	ctx.JSON(http.StatusOK, gin.H{
-		"title":    "Hello World",
-		"subtitle": "Belajar golang bareng Reza Alfara",
+		"data": bookResponse,
 	})
 }
 
-func (h *bookHandler) BooksHandler(ctx *gin.Context) {
-	id := ctx.Param("id")
-	title := ctx.Param("title")
-
-	ctx.JSON(http.StatusOK, gin.H{"id": id, "title": title})
-}
-
-func (h *bookHandler) QueryHandler(ctx *gin.Context) {
-	title := ctx.Query("title")
-	price := ctx.Query("price")
-
-	ctx.JSON(http.StatusOK, gin.H{"title": title, "price": price})
-}
-
-func (h *bookHandler) PostBooksHandler(ctx *gin.Context) {
+func (h *bookHandler) CreateBook(ctx *gin.Context) {
 	var bookRequest book.BookRequest
 
 	err := ctx.ShouldBindJSON(&bookRequest)
@@ -77,7 +91,76 @@ func (h *bookHandler) PostBooksHandler(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{
-		"data": book,
+		"data": convertToBookRespone(book),
 	})
 
+}
+
+func (h *bookHandler) UpdateBook(ctx *gin.Context) {
+	var bookRequest book.BookRequest
+
+	err := ctx.ShouldBindJSON(&bookRequest)
+	if err != nil {
+		if validationErrs, ok := err.(validator.ValidationErrors); ok {
+			// Handle validation errors
+			var errorMessages []string
+			for _, e := range validationErrs {
+				errorMessage := fmt.Sprintf("Error on field %s, condition: %s", e.Field(), e.ActualTag())
+				errorMessages = append(errorMessages, errorMessage)
+			}
+			ctx.JSON(http.StatusBadRequest, gin.H{"errors": errorMessages})
+			return
+		}
+
+		// Handle other errors
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	idString := ctx.Param("id")
+	id, _ := strconv.Atoi(idString)
+
+	book, err := h.bookService.Update(id, bookRequest)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": convertToBookRespone(book),
+	})
+
+}
+
+func (h *bookHandler) DeleteBook(ctx *gin.Context) {
+	idString := ctx.Param("id")
+	id, _ := strconv.Atoi(idString)
+
+	b, err := h.bookService.Delete(id)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"errors": err,
+		})
+		return
+	}
+
+	bookResponse := convertToBookRespone(b)
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": bookResponse,
+	})
+}
+
+func convertToBookRespone(b book.Book) book.BookResponse {
+	return book.BookResponse{
+		ID:          b.ID,
+		Title:       b.Title,
+		Price:       b.Price,
+		Description: b.Description,
+		Rating:      b.Rating,
+		Discount:    b.Discount,
+	}
 }
--- main.go
+++ main.go
@@ -28,11 +28,11 @@ func main() {
 
 	v1 := router.Group("/v1")
 
-	v1.GET("/", bookHandler.RootHandler)
-	v1.GET("/hello", bookHandler.HelloHandler)
-	v1.GET("/books/:id/:title", bookHandler.BooksHandler)
-	v1.GET("/query", bookHandler.QueryHandler)
-	v1.POST("/books", bookHandler.PostBooksHandler)
+	v1.GET("/books", bookHandler.GetBooks)
+	v1.GET("/books/:id", bookHandler.GetBook)
+	v1.POST("/books", bookHandler.CreateBook)
+	v1.PUT("/books/:id", bookHandler.UpdateBook)
+	v1.DELETE("/books/:id", bookHandler.DeleteBook)
 
 	router.Run()
 
